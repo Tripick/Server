251c1340fb19873ef4571046fb8c023f73589550
Vahradrim <vahradrim@gmail.com>
2021-02-22 15:24:35 -0500

    PlaceDescription

---
+++ Migrations/20210222202334_PlaceDescription.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TripickServer.Migrations
+{
+    public partial class PlaceDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Places",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Places");
+        }
+    }
+}
